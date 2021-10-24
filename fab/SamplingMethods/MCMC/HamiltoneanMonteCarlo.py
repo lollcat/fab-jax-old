63019ba8d3ea3a83b0648ca200f23d9a77494ff4
lollcat <l.midgley@instadeep.com>
2021-10-25 00:09:04 +0100

    testing almost done

--- fab/SamplingMethods/MCMC/HamiltoneanMonteCarlo.py
+++ fab/SamplingMethods/MCMC/HamiltoneanMonteCarlo.py
@@ -1,23 +1,38 @@
 import jax.numpy as jnp
 import jax
 import optax
-from dataclasses import dataclass
+from typing import NamedTuple
 from functools import partial
 from fab.types import Params
 import chex
 
-# TODO: ask alex
 
-@dataclass
-class HMCStatePAccept:
+# TODO: types
+
+
+
+class NoGradParams(NamedTuple):
+    std_devs: jnp.ndarray # per dist, per element
+
+
+class HMCStatePAccept(NamedTuple):
     # these params are the step sizes
-    step_size_params: jnp.ndarray = jnp.array(1.0)
+    no_grad_params: NoGradParams  # additional params not directly used to compute step size
+    step_size_params: jnp.ndarray
 
-@dataclass
-class HMCStateGradientBased:
+
+class HMCStateGradientBased(NamedTuple):
+    no_grad_params: NoGradParams  # additional params not directly used to compute step size
     step_size_params: Params
-    no_grad_params: Params
     optimizer_state: optax.OptState
+
+
+class Info(NamedTuple):
+    average_acceptance_probabilities_per_outer_loop: jnp.ndarray
+    std_per_element: jnp.ndarray
+    mean_delta_per_element: jnp.ndarray
+    expected_scaled_mean_distance_per_outer_loop: jnp.ndarray
+
 
 
 
@@ -37,6 +52,10 @@ class HamiltoneanMonteCarlo:
         self._initial_step_size = initial_step_size
         if self.step_tuning_method == "gradient_based":
             self.lr = lr
+        elif self.step_tuning_method == "p_accept":
+            self.target_p_accept = 0.65
+        else:
+            raise NotImplementedError
 
         def U(learnt_dist_params, q, i):
             j = i + 1  # j is loop iter param in AnnealedImportanceSampling.py
@@ -48,6 +67,8 @@ class HamiltoneanMonteCarlo:
         self.grad_U = grad_U
 
     def get_init_state(self):
+        no_grad_params = NoGradParams(jnp.ones((
+            self.n_intermediate_distributions, self.dim)))
         if self.step_tuning_method == "gradient_based":
             step_params = {}
             for i in range(self.n_intermediate_distributions):
@@ -56,13 +77,11 @@ class HamiltoneanMonteCarlo:
                 step_params[i] = step_params_per_dist
             self.optimizer = optax.adam(self.lr)
             initial_opt_state = self.optimizer.init(step_params)
-            no_grad_params = {"stds_per_dist_per_dim": jnp.ones((
-                self.n_intermediate_distributions,  self.n_outer_steps, self.dim))}
-            initial_state = HMCStateGradientBased(step_params, no_grad_params, initial_opt_state)
+            initial_state = HMCStateGradientBased(no_grad_params, step_params, initial_opt_state)
         elif self.step_tuning_method == "p_accept":
             step_size = jnp.ones((self.n_intermediate_distributions,  self.n_outer_steps))*\
                         self._initial_step_size
-            initial_state = HMCStatePAccept(step_size)
+            initial_state = HMCStatePAccept(no_grad_params, step_size)
         else:
             raise NotImplemented
         return initial_state
@@ -78,7 +97,7 @@ class HamiltoneanMonteCarlo:
 
     @staticmethod
     def _inner_most_loop_func(grad_U, L, learnt_dist_params, i, carry, xs):
-        p, q = carry
+        p, q = jax.lax.stop_gradient(carry)  # ensure only grad to epsilon for final iter
         l, epsilon = xs
         q = q + epsilon * p
         p = jax.lax.cond(l != (L - 1), lambda p: p - epsilon * grad_U(learnt_dist_params, q, i),
@@ -88,7 +107,7 @@ class HamiltoneanMonteCarlo:
 
     @staticmethod
     def _outer_loop_func(U, grad_U, inner_most_loop_func, L, learnt_dist_params, i, carry, xs):
-        current_q = carry
+        current_q = jax.lax.stop_gradient(carry)
         rng_key = xs["rng_key"]
         epsilon = xs["step_size"]
         q = current_q
@@ -133,11 +152,11 @@ class HamiltoneanMonteCarlo:
 
 
     def get_key_info(self, x_batch_initial, current_q_per_outer_loop,
-                     acceptance_probabilities_per_outer_loop):
+                     acceptance_probabilities_per_outer_loop,
+                     transition_operator_additional_state_info, i) -> Info:
         """Info for calculating losses, and for inspections"""
-        info = {}
         # average over batch
-        info["average_acceptance_probabilities_per_outer_loop"] = jnp.mean(
+        average_acceptance_probabilities_per_outer_loop = jnp.mean(
             acceptance_probabilities_per_outer_loop, axis=0)
 
         # calculate distance moved
@@ -147,62 +166,99 @@ class HamiltoneanMonteCarlo:
                                                                  ...
                                                                  ]], axis=1))
         delta_per_element = current_q_per_outer_loop - starting_points
-        distance_per_batch_per_outer_loop = jnp.linalg.norm(delta_per_element, ord=2, axis=-1)
-        mean_distance_per_outer_loop = jnp.mean(distance_per_batch_per_outer_loop, axis=0)
-        info["mean_distance_per_outer_loop"] = mean_distance_per_outer_loop
+        mean_delta_per_outer_loop_per_element = jnp.mean(jnp.abs(delta_per_element), axis=0)  # save
+        # for
+        # logging
+        std_per_element = jnp.std(current_q_per_outer_loop[:, -1, :], axis=0)
         # distance with std scaling
-        # TODO
-        return info
+        stds = transition_operator_additional_state_info.std_devs[i][None, None, :]
+        chex.assert_equal_rank([delta_per_element, stds])  # check broadcasting will be fine
+        squared_scaled_distance = jnp.sum((delta_per_element / stds)**2, axis=-1)
+        chex.assert_equal_rank([acceptance_probabilities_per_outer_loop,
+                                squared_scaled_distance]) # check safe broadcasting
+        expected_scaled_mean_distance_per_outer_loop = \
+            acceptance_probabilities_per_outer_loop * squared_scaled_distance
+
+        return Info(average_acceptance_probabilities_per_outer_loop,
+                    std_per_element,
+                    mean_delta_per_outer_loop_per_element,
+                    expected_scaled_mean_distance_per_outer_loop)
 
 
     def run_and_loss(self, key, learnt_distribution_params,
-                     transition_operator_step_sizes, transition_operator_additional_state_info,
+                     transition_operator_step_size_params, transition_operator_additional_state_info,
                      x_batch, i):
         seeds = jax.random.split(key, self.batch_size)
-        step_size = self.get_step_size_param_for_dist(transition_operator_step_sizes, i)
+        step_size = self.get_step_size_param_for_dist(transition_operator_step_size_params, i)
         x_batch_final, (current_q_per_outer_loop, acceptance_probabilities_per_outer_loop) = \
             self.run(seeds, learnt_distribution_params,
                      step_size, x_batch, i)
         info = self.get_key_info(x_batch, current_q_per_outer_loop,
-                                 acceptance_probabilities_per_outer_loop)
+                                 acceptance_probabilities_per_outer_loop,
+                                 transition_operator_additional_state_info, i)
         if self.step_tuning_method == "p_accept":
             return x_batch_final, info
         elif self.step_tuning_method == "gradient_based":
-            p_weighted_scaled_distance_squared = acceptance_probabilities_per_outer_loop *\
-                                         info["mean_distance_per_outer_loop"]  / \
-                              transition_operator_additional_state_info["stds_per_dist_per_dim"][i]
-            loss = jnp.mean(1.0 / p_weighted_scaled_distance_squared -
-                            p_weighted_scaled_distance_squared)
+            # prevent nan from 1/0.0
+            expected_scaled_msd = jnp.where(
+                info.expected_scaled_mean_distance_per_outer_loop == 0.0,
+                jnp.ones_like(info.expected_scaled_mean_distance_per_outer_loop)*0.1,
+                info.expected_scaled_mean_distance_per_outer_loop
+                                            )
+            loss = jnp.mean(1.0 / expected_scaled_msd  -
+                            expected_scaled_msd)
             return loss, (x_batch_final, info)
         else:
             raise NotImplementedError
 
-    def get_interesting_info(self, info):
-        return info
+    def get_interesting_info(self, info) -> dict:
+        return info._asdict()
 
-    def update_no_grad_params(self, i, no_grad_params, info):
+    def update_no_grad_params(self, i, no_grad_params, info) -> NoGradParams:
         return no_grad_params
+
+    def update_step_size_p_accept(self, step_size_params,
+                              average_acceptance_probabilities_per_outer_loop, i):
+        multiplying_factor = jnp.where(average_acceptance_probabilities_per_outer_loop >
+                                  self.target_p_accept, jnp.array(1.1),
+                                       jnp.array(1.0)/1.1
+                                  )
+        chex.assert_equal_shape([step_size_params[i], multiplying_factor])
+        step_size_params = \
+            step_size_params.at[i].set(step_size_params[i]*multiplying_factor)
+        return step_size_params
+
+
 
     def vectorised_run_with_update(self, key, learnt_distribution_params,
                        transition_operator_state, x_batch, i):
         if self.step_tuning_method == "p_accept":
             x_out, info = self.run_and_loss(key, learnt_distribution_params,
-                                            transition_operator_state, x_batch, i)
+                                            transition_operator_state.step_size_params,
+            transition_operator_state.no_grad_params, x_batch, i)
             # tune step size to reach target of p_accept = 0.65
-            new_transition_operator_state = transition_operator_state
+            new_step_size_params = self.update_step_size_p_accept(
+                transition_operator_state.step_size_params,
+                info.average_acceptance_probabilities_per_outer_loop,
+            i)
+            no_grad_params = self.update_no_grad_params(i,
+                                                        transition_operator_state.no_grad_params,
+                                                        info)
+            new_transition_operator_state = HMCStatePAccept(no_grad_params, new_step_size_params)
         else:
-            (loss, x_out, info), grads = jax.value_and_grad(self.run_and_loss,
+            (loss, (x_out, info)), grads = jax.value_and_grad(self.run_and_loss,
                                                             has_aux=True, argnums=2)(
                 key, learnt_distribution_params, transition_operator_state.step_size_params,
                 transition_operator_state.no_grad_params, x_batch, i)
             updates, new_opt_state = self.optimizer.update(grads,
-                                                           transition_operator_state.opt_state)
+                                                           transition_operator_state.optimizer_state)
             step_size_params_new = optax.apply_updates(transition_operator_state.step_size_params,
             updates)
             no_grad_params = self.update_no_grad_params(i, transition_operator_state.no_grad_params,\
                              info)
-            new_transition_operator_state = HMCStateGradientBased(step_size_params_new,
-                                                                  no_grad_params, new_opt_state)
+            new_transition_operator_state = HMCStateGradientBased(no_grad_params,
+                                                                  step_size_params_new,
+                                                                  new_opt_state)
 
         interesting_info = self.get_interesting_info(info)
         return x_out, new_transition_operator_state, interesting_info
--- fab/SamplingMethods/MCMC/HamiltoneanMonteCarlo_test.py
+++ fab/SamplingMethods/MCMC/HamiltoneanMonteCarlo_test.py
@@ -3,7 +3,9 @@ import jax.numpy as jnp
 from absl.testing import absltest
 import jax
 import haiku as hk
+import numpy as np
 from functools import partial
+import distrax
 
 from fab.SamplingMethods.MCMC.HamiltoneanMonteCarlo import HamiltoneanMonteCarlo, \
     HMCStateGradientBased, HMCStatePAccept
@@ -13,8 +15,12 @@ from fab.SamplingMethods.MCMC.HamiltoneanMonteCarlo import HamiltoneanMonteCarlo
 class Test_HMC(absltest.TestCase):
     dim = 2
     n_intermediate_distributions = 2
-    intermediate_target_log_prob_fn = lambda params, x, j: jnp.exp(-jnp.sum(x**2) + jnp.sum(x))
-    batch_size = 5
+    target_distribution = distrax.MultivariateNormalDiag(jnp.zeros((dim,)),
+                                                         jnp.ones((dim,)))
+    intermediate_target_log_prob_fn = lambda params, x, j: \
+        distrax.MultivariateNormalDiag(jnp.zeros((x.shape[-1], )), jnp.ones((x.shape[-1],
+                                                                             ))).log_prob(x)
+    batch_size = 30
     n_outer_steps = 2
     n_inner_steps = 4
     HMC_p_accept = HamiltoneanMonteCarlo(dim, n_intermediate_distributions,
@@ -40,7 +46,7 @@ class Test_HMC(absltest.TestCase):
         initial_state = self.HMC_grad_based.get_init_state()
         assert isinstance(initial_state, HMCStateGradientBased)
 
-    def test__get_step_size_param_for_dist(self):
+    def test__get_step_size_param_for_dist(self) -> jnp.ndarray:
         for i in range(self.n_intermediate_distributions):
             # p_accept HMC
             step_size_param = self.HMC_p_accept.get_step_size_param_for_dist(
@@ -142,23 +148,147 @@ class Test_HMC(absltest.TestCase):
                                                                     self.n_outer_steps))
 
 
-    def test__run_and_los(self):
+    def test__run_and_loss(self):
+        key = next(self.rng)
+        learnt_distribution_param = None
+        x = jax.random.normal(shape=(self.batch_size, self.dim), key=next(self.rng))
+        i = 0
+        # p_accept
+        transition_operator_step_sizes = self.initial_state_p_accept.step_size_params
+        transition_operator_additional_state_info = self.initial_state_p_accept.no_grad_params
+        x_batch_final, info = self.HMC_p_accept.run_and_loss(key, learnt_distribution_param,
+                                                             transition_operator_step_sizes,
+                                                             transition_operator_additional_state_info,
+                                                             x, i)
+        chex.assert_equal_shape([x, x_batch_final])
+        assert (x != x_batch_final).any()  # confirm at least one movement
+        chex.assert_shape(info.expected_scaled_mean_distance_per_outer_loop, (self.batch_size,
+                                                                              self.n_outer_steps))
+        chex.assert_shape(info.average_acceptance_probabilities_per_outer_loop,
+                          (self.n_outer_steps,))
+        chex.assert_shape(info.mean_delta_per_element, (self.n_outer_steps, self.dim))
+        chex.assert_shape(info.std_per_element, (self.dim,))
 
+        # gradient_based
+        transition_operator_step_sizes = self.initial_state_grad_based.step_size_params
+        transition_operator_additional_state_info = self.initial_state_grad_based.no_grad_params
+        loss, (x_batch_final, info) = self.HMC_grad_based.run_and_loss(key,
+        learnt_distribution_param,
+                                                             transition_operator_step_sizes,
+                                                             transition_operator_additional_state_info,
+                                                             x, i)
+        chex.assert_shape(loss, ())
+        assert jnp.isfinite(loss)
+        chex.assert_equal_shape([x, x_batch_final])
+        assert (x != x_batch_final).any()  # confirm at least one movement
+        chex.assert_shape(info.expected_scaled_mean_distance_per_outer_loop, (self.batch_size,
+                                                                              self.n_outer_steps))
+        chex.assert_shape(info.average_acceptance_probabilities_per_outer_loop,
+                          (self.n_outer_steps,))
+        chex.assert_shape(info.mean_delta_per_element, (self.n_outer_steps, self.dim))
+        chex.assert_shape(info.std_per_element, (self.dim,))
+
+
+    def test__vectorised_run_with_updates(self):
+        key = next(self.rng)
+        learnt_distribution_param = None
+        x = jax.random.normal(shape=(self.batch_size, self.dim), key=next(self.rng))
+        i = 0
+        # p_accept
+        transition_operator_state = self.initial_state_p_accept
+        x_out, new_transition_operator_state, interesting_info = \
+            self.HMC_p_accept.vectorised_run_with_update(
+                key, learnt_distribution_param, transition_operator_state, x, i)
+        assert isinstance(new_transition_operator_state, HMCStatePAccept)
+        assert isinstance(interesting_info, dict)
+        chex.assert_equal_shape([x, x_out])
+        # gradient based
+        transition_operator_state = self.initial_state_grad_based
+        x_out, new_transition_operator_state, interesting_info = \
+            self.HMC_grad_based.vectorised_run_with_update(
+                key, learnt_distribution_param, transition_operator_state, x, i)
+        assert isinstance(new_transition_operator_state, HMCStateGradientBased)
+        assert isinstance(interesting_info, dict)
+        chex.assert_equal_shape([x, x_out])
 
 
 
     def test__with_plot(self):
-        if self.run_plot_test:
-            pass # TODO
+        if not self.run_plot_test:
+            import matplotlib.pyplot as plt
+            key = next(self.rng)
+            target_samples = np.array(self.target_distribution.sample(seed=key, sample_shape=(
+                self.batch_size,)))
+            learnt_distribution_param = None
+            x_init = jax.random.normal(shape=(self.batch_size, self.dim), key=next(self.rng))*0.1
+            i = 0
+            n_loops = 10
+            for name, transition_operator_state, HMC_class in [
+                ("p_accept", self.initial_state_p_accept, self.HMC_p_accept),
+                ("gradient_based", self.initial_state_grad_based, self.HMC_grad_based)]:
+                jitted_transition = jax.jit(HMC_class.vectorised_run_with_update,
+                                            static_argnums=4)
+                x = x_init
+                mean_p_accept_hist = []
+                step_size_hist = []
+                mean_delta_per_element_hist = []
+                expected_scaled_mean_distance_per_outer_loop_hist = []
+                for _ in range(n_loops):
+                    x, transition_operator_state, interesting_info = jitted_transition(
+                       key, learnt_distribution_param, transition_operator_state, x, i)
+                    mean_p_accept = list(interesting_info[
+                        "average_acceptance_probabilities_per_outer_loop"])
+                    mean_p_accept_hist.append(mean_p_accept)
+                    step_size_hist.append(list(HMC_class.get_step_size_param_for_dist(
+                        transition_operator_state.step_size_params, i)))
+                    mean_delta_per_element_hist.append(list(
+                        interesting_info["mean_delta_per_element"]))
+                    expected_scaled_mean_distance_per_outer_loop_hist.append(list((
+                        interesting_info["expected_scaled_mean_distance_per_outer_loop"])))
+                step_size_hist = np.array(step_size_hist)
+                expected_scaled_mean_distance_per_outer_loop_hist = \
+                    np.array(expected_scaled_mean_distance_per_outer_loop_hist)
+                mean_delta_per_element_hist = np.array(mean_delta_per_element_hist)
+                x_init = np.array(x_init)
+                x = np.array(x)
+                fig, axs = plt.subplots(3)
+                axs[0].plot(x_init[:, 0], x_init[:, 1], "o")
+                axs[0].set_title("initial samples")
+                axs[1].plot(x[:, 0], x[:, 1], "o")
+                axs[1].set_title("HMC samples")
+                axs[2].plot(target_samples[:, 0], target_samples[:, 1], "p")
+                axs[2].set_title("target samples")
+                plt.tight_layout()
+                plt.show()
+
+                plt.plot(mean_p_accept_hist)
+                plt.title("p_accept_history")
+                plt.show()
+
+                if name == "p_accept":
+                    plt.plot(step_size_hist)
+                    plt.title("step size history")
+                    plt.show()
+                else:
+                    plt.plot(step_size_hist[:, :, 0], "--", label="dim1")
+                    plt.plot(step_size_hist[:, :, 1], label="dim2")
+                    plt.legend()
+                    plt.title("step size history")
+                    plt.show()
+
+                plt.plot(mean_delta_per_element_hist[:, :, 0], "--", label="dim1")
+                plt.plot(mean_delta_per_element_hist[:, :, 1], label="dim2")
+                plt.legend()
+                plt.title("movement per dim history")
+                plt.show()
+
+                # TODO expected_scaled_mean_distance_per_outer_loop_hist shapes seem wierd
+                # inspect and plot
         else:
             pass
 
 
     # TODO: eyeball the algorithm to double check all the steps are correct
-
-
-
-
 
 
 if __name__ == '__main__':
