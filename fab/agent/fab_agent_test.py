9ed69624034b57d57ec02580a1a5622ba1bc1b8c
lollcat <l.midgley@instadeep.com>
2022-05-02 13:35:02 +0100

    feat: initial setup of replay buffer

--- fab/agent/fab_agent_test.py
+++ fab/agent/fab_agent_test.py
@@ -8,6 +8,7 @@ from fab.learnt_distributions.real_nvp import make_realnvp_dist_funcs
 from fab.target_distributions.many_well import ManyWellEnergy
 from fab.agent.fab_agent import AgentFAB
 from fab.utils.plotting import plot_history, plot_marginal_pair, plot_contours_2D
+from fab.utils.replay_buffer import ReplayBuffer
 
 
 def plotter(fab_agent, log_prob_2D):
@@ -20,60 +21,56 @@ def plotter(fab_agent, log_prob_2D):
         x_base, log_q_x_base = fab_agent.learnt_distribution.sample_and_log_prob.apply(
             state.learnt_distribution_params, rng=state.key,
             sample_shape=(batch_size,))
-        x_ais_loss, _, _, _ = \
+        x_ais_target, _, _, _ = \
             fab_agent.annealed_importance_sampler.run(
                 x_base, log_q_x_base, state.key,
                 state.transition_operator_state,
                 base_log_prob=base_log_prob,
                 target_log_prob=target_log_prob
             )
-        x_ais_target, _, _, _ = \
-            fab_agent.annealed_importance_sampler.run(
-                x_base, log_q_x_base, state.key,
-                state.transition_operator_state,
-                base_log_prob=base_log_prob,
-                target_log_prob=fab_agent.target_log_prob
-            )
-        return x_base, x_ais_loss, x_ais_target
+        return x_base, x_ais_target
 
-    x_base, x_ais_loss, x_ais_target = get_info(fab_agent.state)
-    fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+    x_base, x_ais_target = get_info(fab_agent.state)
+    fig, axs = plt.subplots(1, 2, figsize=(8, 4))
     plot_contours_2D(log_prob_2D, ax=axs[0], bound=3, levels=20)
     plot_marginal_pair(x_base, ax=axs[0])
     axs[0].set_title("base samples")
     plot_contours_2D(log_prob_2D, ax=axs[1], bound=3, levels=20)
-    plot_marginal_pair(x_ais_loss, ax=axs[1])
-    axs[1].set_title("p^2 / q samples")
-    plot_contours_2D(log_prob_2D, ax=axs[2], bound=3, levels=20)
-    plot_marginal_pair(x_ais_target, ax=axs[2])
-    axs[2].set_title("ais target samples")
+    plot_marginal_pair(x_ais_target, ax=axs[1])
+    axs[1].set_title("ais target samples")
     plt.show()
     return [fig]
 
 
 class Test_AgentFAB(absltest.TestCase):
     # jax.config.update("jax_enable_x64", True)
-    dim = 4
+    dim = 2
     flow_num_layers = 5
-    mlp_hidden_size_per_x_dim = 1
+    mlp_hidden_size_per_x_dim = 5
     real_nvp_flo = make_realnvp_dist_funcs(dim, flow_num_layers,
                                            mlp_hidden_size_per_x_dim=mlp_hidden_size_per_x_dim)
     target = ManyWellEnergy(dim=dim)
     target_log_prob = target.log_prob
     log_prob_2D = target.log_prob_2D
     batch_size = 64
-    n_iter = int(5e3)
+    n_iter = int(1e3)
     loss_type = "alpha_2_div"  # "forward_kl"  "alpha_2_div"
-    style = "proptoloss"  # "vanilla"  "proptoloss"
-    n_intermediate_distributions: int = 4
+    style = "vanilla"  # "vanilla"  "proptoloss"
+    n_intermediate_distributions: int = 2
     soften_ais_weights = False
     use_reparam_loss = False
     max_grad_norm = None
-    lr = 1e-3
-    n_plots = 5
-    n_evals = 10
+    lr = 5e-4
+    n_plots = 3
+    n_evals = 4
     eval_batch_size = batch_size
 
+
+    buffer = ReplayBuffer(dim=dim,
+                          max_length=batch_size*100,
+                          min_sample_length=batch_size*10)
+    n_buffer_updates_per_forward = 4
+    # buffer = None
     # AIS_kwargs = {"additional_transition_operator_kwargs": {"step_tuning_method": "p_accept"}}
     AIS_kwargs = {"transition_operator_type": "hmc_tfp"}  #  "hmc_tfp", "nuts_tfp"
 
@@ -87,6 +84,8 @@ class Test_AgentFAB(absltest.TestCase):
     fab_agent = AgentFAB(learnt_distribution=real_nvp_flo,
                          target_log_prob=target_log_prob,
                          n_intermediate_distributions=n_intermediate_distributions,
+                         replay_buffer=buffer,
+                         n_buffer_updates_per_forward=n_buffer_updates_per_forward,
                          AIS_kwargs=AIS_kwargs,
                          optimizer=optimizer,
                          loss_type=loss_type,
